badd4b79850fe34c8a564451376d774d8b2fb1c9
Milad Azami <miladazami120@gmail.com>
2019-12-09 02:08:21 +0330

    referral_code() function modified.

--- Nobitex.py
+++ Nobitex.py
@@ -49,7 +49,10 @@ def profile():
             print(f"Profile: \n{response.json()}")
             # print(f"status code = {response.status_code}")
     except requests.exceptions.RequestException as error:
-        print(f"ERROR! \n{error}")
+        if response.status_code == 401:
+            print(f"ERROR! \nplease login then try again. \n{error}")
+        else:
+            print(f"ERROR! \n{error}")
 
 def list_of_orders(type, srcCurrency = None, dstCurrency = "usdt", order = "-price"):
     # Use this function to get the list of orders.
@@ -180,21 +183,25 @@ def login_attempts():
             print(f"ERROR! \n{error}")
 
 def referral_code():
+    # Use this function to get referral code.
     open_token = open("token.txt", "r")
     token = open_token.read()
     header = {"Authorization": "Token " + token}
+    open_token.close()
     try:
         response = requests.post(
-            url=URL + "/users/get-referral-code",
-            headers=header
+            url = URL + "/users/get-referral-code",
+            headers = header
         )
         response.raise_for_status()
         if response.status_code == 200:
             print(f"Your referral code: \n{response.json()['referralCode']}")
-            open_token.close()
             # print(f"status code = {response.status_code}")
-    except requests.exceptions.RequestException as error2:
-        print(f"ERROR! \n{error2}")
+    except requests.exceptions.RequestException as error:
+        if response.status_code == 401:
+            print(f"ERROR! \nplease login then try again. \n{error}")
+        else:
+            print(f"ERROR! \n{error}")
 
 def add_card_number(card_number, bank_name):
     open_token = open("token.txt", "r")
@@ -393,3 +400,23 @@ def order(type, srcCurrency, dstCurrency, amount, price, execution = "limit"):
     except requests.exceptions.RequestException as error2:
         print(f"ERROR! \n{error2}")
 
+
+# login("miladazami120@gmail.com", "Sa3257121600", "yes")
+# profile()
+# list_of_orders("buy")
+# list_of_trades("btc", "rls")
+# nobitex_statistics("btc", "usdt")
+## OHLC("btcusdt, "h", "1567424381", "1567395581")
+# print(global_statistics())
+# login_attempts()
+# referral_code()
+## add_card_number("5041721011111111", "رسالت")
+## add_account_number("5041721011111111", "IR111111111111111111111111", "رسالت")
+## profile()
+# limitations()
+# wallets_list()
+# wallets_balance("ltc")
+# transactions_lits("3630")
+# deposit_withdraw("3630")
+# generate_address("3630")
+# order("buy", "eth", "rls", "5", "20000000")
